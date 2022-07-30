9e818d969ac661739c8ff5281718b2cdefb8c852
hayongwoon <a01046641857@gmail.com>
2022-07-30 15:40:45 +0900

    push test

--- user/serializers.py
+++ user/serializers.py
@@ -1,4 +1,3 @@
-from django.contrib.auth import authenticate
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 from user.models import User as UserModel
